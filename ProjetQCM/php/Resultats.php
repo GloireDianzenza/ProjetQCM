24955aea6352f446469c780d1b4b92a1b5d7f280
GloireDianzenza <99244798+GloireDianzenza@users.noreply.github.com>
2022-03-09 17:50:38 +0100

    Add files via upload

--- ProjetQCM/php/Resultats.php
+++ ProjetQCM/php/Resultats.php
@@ -16,41 +16,96 @@
         echo '<br>';
         echo '<h1 class="container-fluid">Vous avez obtenu un total de :</h1>'.'<br>';
         echo '<input type="button" value="Retour à la page des questionnaires" id="btnRetour">'.'<br>';
+        include 'cnx.php';
+        $sql = $cnx->prepare('SELECT idQuestionnaire,libelleQuestionnaire FROM questionnaire WHERE libelleQuestionnaire = "Cinéma"');
+        $sql->execute();
+        foreach($sql->fetchAll(PDO::FETCH_NUM) as $test)
+        {
+            $idq = $test[0];
+            $lib = $test[1];
+        }
+        $sql2 = $cnx->prepare('SELECT idQuestionnaire,idQuestion FROM questionquestionnaire WHERE idQuestionnaire = '.$idq);
+        $sql2->execute();
+        $questions = array();
+        foreach($sql2->fetchAll(PDO::FETCH_NUM) as $test2)
+        {
+            array_push($questions,$test2[1]);
+        }
         echo '<br>';
-        echo '<div class="container">';
-            echo '<p class="highlight">Question numéro </p>'.'<br>';
-            echo '<p>Quelle est la question ?</p>'.'<br>';
-            echo '<div class="rad">';
-                echo '<input type="radio" name="reponse"><label  class="correct">&thinsp;Réponse 1</label>'.'<br>';
-                echo '<input type="radio" name="reponse"><label class="wrong">&thinsp;Réponse 2</label>';
-            echo '</div>';
+        foreach($questions as $l)
+        {
+            $qu = $cnx->prepare('SELECT idQuestion,libelleQuestion,nbBonneReponse FROM question WHERE idQuestion = '.$l);
+            $qu->execute();
+            foreach($qu->fetchAll(PDO::FETCH_NUM) as $test3)
+            {
+                $phrase = $test3[1];
+            }
+            $sql3 = $cnx->prepare('SELECT idQuestion,idReponse,ordre,bonne FROM questionreponse WHERE idQuestion = '.$l);
+            $sql3->execute();
+            $reponses = array();
+            foreach($sql3->fetchAll(PDO::FETCH_NUM) as $test4)
+            {
+                array_push($reponses,$test4[1]);
+            }
             echo '<br>';
-            echo '<p>La réponse à la question &thinsp;'.''.' est </p>';
-        echo '</div>';
-        echo '<br>';
-        echo '<div class="container">';
-            echo '<p class="highlight">Question numéro </p>'.'<br>';
-            echo '<p>Quelle est la question ?</p>'.'<br>';
-            echo '<div class="check">';
-                echo '<input type="checkbox" name="test" id=""><label class="correct">&thinsp;Réponse 1</label>'.'<br>';
-                echo '<input type="checkbox" name="test" id=""><label class="correct">&thinsp;Réponse 2</label>'.'<br>';
+            echo '<div class"container">';
+                echo '<p class="highlight">Question numéro '.$l.'</p><br>';
+                echo '<p>'.$phrase.'</p>'.'<br>';
+                if($test3[2] == 1)
+                {
+                    $bonnereponse = 0;
+                    echo '<div class="rad">';
+                        foreach($reponses as $rep)
+                        {
+                            $r = $cnx->prepare('SELECT reponse.idReponse,bonne,valeur FROM questionreponse JOIN reponse ON questionreponse.idReponse = reponse.idReponse WHERE reponse.idReponse = '.$rep.' AND bonne = 1');
+                            $r->execute();
+                            foreach($r->fetchAll(PDO::FETCH_NUM) as $answer)
+                            {
+                                $bonnereponse = $answer[2];
+                            }
+
+                            $sql4 = $cnx->prepare('SELECT idReponse,valeur FROM reponse WHERE idReponse = '.$rep);
+                            $sql4->execute();
+                            foreach($sql4->fetchAll(PDO::FETCH_NUM) as $SSS)
+                            {
+                                echo '<input type="radio" name="reponse'.$l.'"><label>&thinsp;'.$SSS[1].'</label>'.'<br>';
+                            }
+                        }
+                    echo '<p>La réponse à la question '.$l.' est '.$bonnereponse.'</p>';
+                    echo "</div>";
+                }
+                else if($test3[2] > 1)
+                {
+                    $bonnereponse = array();
+                    echo '<div class="check">';
+                        foreach($reponses as $rep)
+                        {
+                            $r = $cnx->prepare('SELECT reponse.idReponse,bonne,valeur FROM questionreponse JOIN reponse ON questionreponse.idReponse = reponse.idReponse WHERE reponse.idReponse = '.$rep.' AND bonne = 1');
+                            $r->execute();
+                            foreach($r->fetchAll(PDO::FETCH_NUM) as $answer)
+                            {
+                                array_push($bonnereponse,$answer[2]);
+                            }
+
+                            $sql4 = $cnx->prepare('SELECT idReponse,valeur FROM reponse WHERE idReponse = '.$rep);
+                            $sql4->execute();
+                            foreach($sql4->fetchAll(PDO::FETCH_NUM) as $SSS)
+                            {
+                                echo '<input type="checkbox" name="reponse'.$l.'"><label>&thinsp;'.$SSS[1].'</label>'.'<br>';
+                            }
+                        }
+                        $conclusion = '<p>Réponse(s) exacte(s) : ';
+                        foreach($bonnereponse as $a)
+                        {
+                            $conclusion = $conclusion.$a.'/';
+                        }
+                        $conclusion = $conclusion.'</p>';
+                        echo $conclusion;
+                    echo "</div>";
+                }
             echo '</div>';
+        }
             echo '<br>';
-            echo '<p>Les réponses à la question &thinsp;'.''.' sont </p>';
-        echo '</div>';
-        echo '<br>';
-        echo '<div class="container">';
-            echo '<p class="highlight">Question numéro </p>'.'<br>';
-            echo '<p>Quelle est la question ?</p>'.'<br>';
-            echo '<div class="check">';
-                echo '<input type="checkbox" name="test" id=""><label class="wrong">&thinsp;Réponse 1</label>'.'<br>';
-                echo '<input type="checkbox" name="test" id=""><label class="correct">&thinsp;Réponse 2</label>'.'<br>';
-                echo '<input type="checkbox" name="test" id=""><label class="wrong">&thinsp;Réponse 3</label>'.'<br>';
-                echo '<input type="checkbox" name="test" id=""><label class="correct">&thinsp;Réponse 4</label>'.'<br>';
-            echo '</div>';
-            echo '<br>';
-            echo '<p>Réponse(s) fausse(s) : '.''.'</p>';
-            echo '<p>Réponse(s) exacte(s) : '.''.'</p>';
         echo '</div>';
         echo '<br>';
         echo '<input type="button" value="Retour à la page des questionnaires" id="btnRetour">'.'<br>';
