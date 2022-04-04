9a0836fa99ed873c0eeaa9210ccba0ad38249ff5
Gloire Dianzenza <gloiredianzenza5@gmail.com>
2022-04-04 10:10:36 +0200

    TEst commit

---
+++ PartieGloire/JS/index.js
@@ -0,0 +1,28 @@
+var reponse = 1;
+
+function AjouterReponse()
+{
+    if(document.getElementById("divAnswers").children.length < 4)
+    {
+        var newText = document.createElement('input');
+        newText.setAttribute('type','text');
+        newText.setAttribute('name','reponse[]');
+        newText.setAttribute('placeholder','Insérer réponse ici');
+        var element = document.getElementById("divAnswers");
+        element.appendChild(newText);
+    }
+}
+
+function EnleverReponse()
+{
+    if(document.getElementById("divAnswers").lastElementChild.tagName == "EDITOR-SQUIGGLER")
+    {
+        var select = document.getElementById("divAnswers");
+        select.removeChild(select.lastChild);
+    }
+    if(document.getElementById("divAnswers").children.length > 0)
+    {
+        var select = document.getElementById("divAnswers");
+        select.removeChild(select.lastChild);
+    }
+}
