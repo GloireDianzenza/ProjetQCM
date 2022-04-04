2cde3cf5934b44cfda4a668be4e0ad3b229dea5b
Gloire Dianzenza <gloiredianzenza5@gmail.com>
2022-04-04 11:43:01 +0200

    maj

--- PartieGloire/PHP/CreationQuestion.php
+++ PartieGloire/PHP/CreationQuestion.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_GET['choixReponse']))
     {
-        header("Location:../PHP/PartieLevi/vuePhp/DefinirReponse.php");
+        header("Location:../../PartieLevi/vuePhp/DefinirReponse.php");
     }
 ?>
 
