80a4c868eedfdb93a8a2f46ff37b104e2ee3367a
LeviWebert <levi.webert@ortmontreuil.fr>
2022-05-03 11:42:44 +0200

    rectification urgente

--- Finale/vuePhp/DefinirReponse.php
+++ Finale/vuePhp/DefinirReponse.php
@@ -15,6 +15,16 @@ $sql->execute();
 $sql=$cnx->prepare("INSERT INTO questionquestionnaire VALUES (".$_SESSION['idQuest'].",".$idQ.")");
 $sql->execute();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choix réponses</title>
+    <script src="../JS/JQuery 3.5.1.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../JS/index.js"></script>
     <script>
         $(
             function()
@@ -28,7 +38,11 @@ $sql->execute();
             
         )
     </script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
     <input id="idQ" value="<?php echo $idQ; ?>" hidden type="text">
+    <form action="" method="get">
         <div>Créer des réponses</div>
         <div id="containerReponse">
         
@@ -37,5 +51,7 @@ $sql->execute();
         <input class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 " type="button" id="ajouterR" value="Ajouter une réponse">
         <!-- <input type="button" onclick="Annuler()" id="EnleverRep" value="Enlever toute les réponses"> -->
         
+    </form>
     
-    
+</body>
+</html>
